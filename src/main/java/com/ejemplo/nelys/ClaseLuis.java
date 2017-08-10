392b56cbed83282b69fc4147a2a039fdbac80aa6
nelys Mollocondo <nelys.mp@gmail.com>
2017-08-10 16:03:35 -0500

    se agrego geter y seter

--- src/main/java/com/ejemplo/nelys/ClaseLuis.java
+++ src/main/java/com/ejemplo/nelys/ClaseLuis.java
@@ -4,21 +4,25 @@ public class ClaseLuis {
 
 	private String nombre;
 	private int edad;
-
+	private int numero;
 	public String getNombre() {
 		return nombre;
 	}
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
 	public int getEdad() {
 		return edad;
 	}
-
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
 
+	
 }
